59f13c07daaf8db92616df49f865f4d8797c73cb
prssvrajusrkr <prssvr@srkrec.ac.in>
2021-05-18 11:46:49 +0530

    javascript

--- mycode.js
+++ mycode.js
@@ -1,4 +1,10 @@
 function mycode()
 {
-  alert("Testing");
+    var a=document.getElementById("tx1").value;
+    var b=document.getElementById("tx2").value;
+    var c=parseInt(a)+parseInt(b);
+    var res=document.getElementById("res");
+    res.innerHTML="Additon of : "+a+"and "+b+" is  : "+c;
+    
+
 }
