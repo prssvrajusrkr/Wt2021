4b0d54542630500a8732d3c0c426465f042b4cdb
prssvrajusrkr <prssvr@srkrec.ac.in>
2021-06-01 11:46:46 +0530

    second update

--- Lab-4/LoginValidation.js
+++ Lab-4/LoginValidation.js
@@ -2,18 +2,18 @@ function  loginFieldsValidate()
 {
    
         var valid=false;
-	var id=document.forms[0].elements[0].value;
+_var id=document.forms[0].elements[0].value;
         var  pass=document.forms[0].elements[1].value;
 
         var  id_re= new RegExp("^[a-z][0-9a-zA-Z]+$","g");
-       	var  pass_re=new RegExp("[0-9A-Za-z]{6,}","g")
+       _var  pass_re=new RegExp("[0-9A-Za-z]{6,}","g")
                
         if(id.match(id_re))
         {
                 if(pass.match(pass_re))
                 {
                         alert("All Field values are in correct format");
-						valid=true;
+______valid=true;
                 }
                 else
                 {
